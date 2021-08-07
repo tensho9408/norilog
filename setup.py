c7e587880f5ec3adc56a19f5505a78e408fc3b7b
tensho9408 <tenshoohashi@gmail.com>
2021-08-07 17:24:41 +0000

    change project name as norilog_tenpy

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="norilog",
+    name="norilog_tenpy",
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
